7e30f8678295e4c309e00000d64a4c991aabe7dd
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-07 15:30:24 +0200

    add utility functions, button component, and global styles; update Tailwind CSS configuration

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const ProfilePage = () => {
     return (
-        <div>ProfilePage</div>
+        <>
+            ProfilePage
+            <Button>Click me</Button>
+        </>
     )
 }
 
