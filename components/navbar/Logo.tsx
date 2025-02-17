715d7a1a84f8f98df44ea3af86d3da2a160559b9
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-17 19:50:05 +0200

    add Logo and NavSearch components; integrate react-icons and input for enhanced navigation

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
+import { FiHome } from 'react-icons/fi'
 
 const Logo = () => {
     return (
-        <div>Logo</div>
+        <div>
+            <Button size='icon' asChild>
+                <Link href='/'>
+                    <FiHome className='w-6 h-6' />
+                </Link>
+            </Button>
+        </div>
     )
 }
 
