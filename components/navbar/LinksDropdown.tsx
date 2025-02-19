9d6cad01aa8fc068037c2a935a87985e9a56f0a7
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-19 18:09:55 +0200

    add LinksDropdown and UserIcon components; implement dropdown functionality and user icon display

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator,
+} from '../../components/ui/dropdown-menu';
+import { LuAlignLeft } from 'react-icons/lu';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import UserIcon from './UserIcon';
+import { links } from '@/utils/links';
+import SignOutLink from './SignOutLink';
 
-const LinksDropdown = () => {
-    return (
-        <div>LinksDropdown</div>
-    )
+function LinksDropdown() {
+    return <div>LinksDropdown</div>;
 }
 
-export default LinksDropdown
+export default LinksDropdown;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,8 +1,11 @@
+import { LuUser } from 'react-icons/lu'
 import React from 'react'
 
 const UserIcon = () => {
     return (
-        <div>UserIcon</div>
+        <>
+            <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />
+        </>
     )
 }
 
