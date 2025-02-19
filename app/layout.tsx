9145a92cf64779642fbdac432cb3f4439154fec3
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-19 16:03:09 +0200

    add theme provider and mode toggle; integrate next-themes for improved dark mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar></Navbar>
+        <Providers>
+          <Navbar></Navbar>
+        </Providers>
         <main className="container py-10">
           {children}
         </main>
