30396bb78e477aded2bf6282d958eb6835ae72a6
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-20 16:37:59 +0200

    refactor LinksDropdown component; enhance dropdown trigger with button styling and improve layout

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -13,15 +13,23 @@ import { links } from '@/utils/links';
 import SignOutLink from './SignOutLink';
 
 function LinksDropdown() {
-    return <DropdownMenu>
-        <DropdownMenuTrigger
-            asChild
-            className='flex gap-4 max-w-[100px]'
-        >
-
-        </DropdownMenuTrigger>
-        <DropdownMenuContent></DropdownMenuContent>
-    </DropdownMenu>;
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='outline' className='flex gap-4 max-w-[100px] bg-red-600'>
+                    <LuAlignLeft className='w-6 h-6' />
+                    <UserIcon />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className='w-52'
+                align='start'
+                sideOffset={10}
+            >
+                
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
 }
 
 export default LinksDropdown;
