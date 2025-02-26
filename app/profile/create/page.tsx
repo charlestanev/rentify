76062f24729ad13732fb5aa7728e7b346de7f8c5
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-26 15:54:04 +0200

    add SubmitButton component; enhance button with loading state and spinner for better user feedback

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -13,7 +13,6 @@ const CreateProfilePage = () => {
             <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
             <div className='border p-8 rounded-md max-w-lg'>
                 <form action={createProfileAction}>
-                   
                     <Button type='submit'>Create Profile</Button>
                 </form>
             </div>
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useFormStatus } from 'react-dom'
 import { Button } from '../ui/button'
+import { Loader2 } from 'lucide-react'
 
 type SubmitButtonProps = {
     className?: string
@@ -17,8 +18,15 @@ export function SubmitButton({ className = "", text = "submit" }: SubmitButtonPr
             type='submit'
             disabled={pending}
             className={`capitalize ${className}`}
+            size='lg'
         >
-            {text}
+            {pending
+                ? <>
+                    <Loader2 className="animate-spin mr-2 h-4 w-4" />
+                    Please wait...
+                </>
+                : text
+            }
         </Button>
     )
 }
