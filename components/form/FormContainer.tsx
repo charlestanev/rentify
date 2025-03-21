8a8b32a88545fc5c6443fa8830af08d51bfe4db3
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-03-21 17:31:00 +0200

    add
    add

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,6 +1,13 @@
 'use client';
-
+import { useToast } from "@/hooks/use-toast";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+import { actionFunction } from 
 
 const initialState = {
     message: '',
 };
+
+function FormContainer({ action, children }: { action: actionFunction, children: React.ReactNode }) {
+    
+}
